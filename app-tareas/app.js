6521e8d782c39f2b668df99cf7cc44a9a437532b
Yuchan <yu.nakasone@gmail.com>
2022-05-05 16:27:00 -0300

    App-Tareas-2 Terminado

--- app-tareas/app.js
+++ app-tareas/app.js
@@ -1,8 +1,15 @@
 const chalk = require('chalk');
 
+function atajarArgsConsola(array){
+    /*  Recibe el array resultante del process.argvs, y devuelve un array de como maximo 3 posiciones. */
+    let [arg0, arg1, arg2, ...arg3] = array;
+    return [arg0, arg1, arg2, arg3.join(" ")];
+}
 
-function menu(objTareas) {
-    switch(process.argv[2]){
+
+function menu(objTareas, argumentos) {
+    /* Recibe un objeto del tipo tarea y un array con los argumentos atajados. Realiza las diferentes funcionalidades solicitadas dependiendo del argumento recidido por consola. */
+    switch(argumentos[2]){
         case "listar":
             let arrayTareas = objTareas.leerArchivo(objTareas.archivo);
             objTareas.mostrarTareas(arrayTareas);
@@ -13,13 +20,12 @@ function menu(objTareas) {
             break;
 
         case "crear":
-            if (process.argv[3] == undefined) {
+            if (argumentos[3] == '') {
                 console.log(chalk.red("*Atención - Debes ingresar el nombre de la nueva tarea. (ej: node app.js crear 'Nueva tarea')\n"));
                 break;
             }
-            // Ver como hacer para poder recibir todos los argumentos siguientes al 3 como 1 solo argumento.
             let nuevaTarea = {
-                titulo: process.argv[3],
+                titulo: argumentos[3],
                 estado: "Pendiente"
             }
             objTareas.guardarTarea(nuevaTarea);
@@ -27,11 +33,11 @@ function menu(objTareas) {
             break;
 
         case "filtrar":
-            if (process.argv[3] == undefined) {
+            if (argumentos[3] == '') {
                 console.log(chalk.red("*Atención - Debes ingresar el estado por el que deseas filtrar. (ej: node app.js filtrar 'Pendiente')\n"));
                 break;
             }
-            let estado = process.argv[3];
+            let estado = argumentos[3];
             objTareas.filtrarPorEstado(estado);
             break;
 
@@ -41,11 +47,10 @@ function menu(objTareas) {
 
 }
 
-
-
 function main(){
     const objTareas = require('./tareas');
-    menu(objTareas);
+    let arrayArgumentos = atajarArgsConsola(process.argv);
+    menu(objTareas, arrayArgumentos);
 }
 
 main();
--- app-tareas/tareas.js
+++ app-tareas/tareas.js
@@ -14,7 +14,7 @@ let objetoTareas = {
     mostrarTareas: function(array) {
         /* Recibe un array de objetos tarea y muestra por consola las tareas que hay en este */
         console.log(chalk.bold("\nListado de tareas\n---------------------"));
-        array.forEach((element, index, array) => { // parametros (elemento actual, indice del elemento actual(OPCIONAL), rray del elemento actual(OPCIONAL))
+        array.forEach((element, index) => { // parametros (elemento actual, indice del elemento actual(OPCIONAL), array del elemento actual(OPCIONAL))
             console.log(index+1 + ". " + chalk.bold(element.titulo) + " - " + chalk.green.italic(element.estado));
         });
         console.log("\n\n");
@@ -26,16 +26,14 @@ let objetoTareas = {
         let datosConvertidos = JSON.stringify(array);
         fs.writeFileSync(this.archivo, datosConvertidos, function(error) {
             if (error) throw error;
-            console.log('Tarea guardada exitosamente!');
+            console.log('Ha ocurrido un error al intentar guardar la tarea');
         });
     },
 
     guardarTarea: function(tarea) {
         /* Recibe un objeto de tipo tarea y lo guarda en el archivo json. */
         let arrayTareas = this.leerArchivo(this.archivo);
-        //console.log("OLD", this.mostrarTareas(arrayTareas));
         arrayTareas.push(tarea);
-        //console.log("UPDATE!", this.mostrarTareas(arrayTareas));
         this.escribirJSON(arrayTareas);
     },
 
@@ -52,48 +50,7 @@ let objetoTareas = {
     /* Recibe un estado (string) y filtra las tareas del archivo JSON por estado y luego las muestra por consola. */
         let tareasFiltradas = this.leerPorEstado(estado);
         this.mostrarTareas(tareasFiltradas);
-    },
-
-    // menuDeAcciones: function() {
-    //     /* Se encarga de ejecutar diferentes acciones sobre dicho array segun los argumentos ingresados por consola. */
-    //     switch(process.argv[2]){
-    //         case "listar":
-    //             let arrayTareas = this.leerArchivo(this.archivo);
-    //             this.mostrarTareas(arrayTareas);
-    //             break;
-
-    //         case undefined:
-    //             console.log(chalk.red("*Atención - Tienes que pasar una acción.\nLas acciones disponibles son: listar\n"));
-    //             break;
-
-    //         case "crear":
-    //             if (process.argv[3] == undefined) {
-    //                 console.log(chalk.red("*Atención - Debes ingresar el nombre de la nueva tarea. (ej: node app.js crear 'Nueva tarea')\n"));
-    //                 break;
-    //             }
-    //             // Ver como hacer para poder recibir todos los argumentos siguientes al 3 como 1 solo argumento.
-    //             let nuevaTarea = {
-    //                 titulo: process.argv[3],
-    //                 estado: "Pendiente"
-    //             }
-    //             //console.log(nuevaTarea);
-    //             this.guardarTarea(nuevaTarea);
-    //             console.log(chalk.green("*Se ha creado la tarea '" + nuevaTarea.titulo + "' exitosamente!\n"));
-    //             break;
-
-    //         case "filtrar":
-    //             if (process.argv[3] == undefined) {
-    //                 console.log(chalk.red("*Atención - Debes ingresar el estado por el que deseas filtrar. (ej: node app.js filtrar 'Pendiente')\n"));
-    //                 break;
-    //             }
-    //             let estado = process.argv[3];
-    //             this.filtrarPorEstado(estado);
-    //             break;
-
-    //         default:
-    //             console.log(chalk.red("*No entiendo qué quieres hacer.\nLas acciones disponibles son: listar\n"));
-    //     } 
-    // }
+    }
 
 }
 
